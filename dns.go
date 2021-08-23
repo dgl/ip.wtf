950b3944b0fa6c3e7939515473b48e8b1c2b48cd
David Leadbeater <dgl@dgl.cx>
2021-08-23 16:48:28 +1000

    Add geolocation, also start of DNS server

--- dns.go
+++ dns.go
@@ -1,43 +1,106 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
-	"runtime/pprof"
 	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/miekg/dns"
 )
 
-const SOA string = "@ SOA ip.d.cx. ip.d.cx. 2021010100 1800 900 0604800 60"
+const port = 8053
 
-// NewRR is a shortcut to dns.NewRR that ignores the error.
-func NewRR(s string) dns.RR { r, _ := dns.NewRR(s); return r }
+var (
+	SOA   = "@ SOA " + *flagHost + ". " + *flagHost + ". 2021010100 1800 900 0604800 60"
+	CNAME = ". 0 CNAME " + *flagHost + "."
+)
 
-func main() {
-    z := "ns.ip.d.cx"
-    
-    rrx := NewRR("$ORIGIN " + z + ".\n" + SOA)
+type DNSData struct {
+	IP         string
+	EdnsSubnet string
+	Expire     time.Time
+}
+
+var dnsMap = map[string]DNSData{}
+
+func dnsHandler(w http.ResponseWriter, req *http.Request) {
+	host := strings.TrimSuffix(req.Host, ".")
+
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD")
+
+	if m, ok := dnsMap[host]; ok {
+		err := json.NewEncoder(w).Encode(m)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	} else {
+		http.Error(w, "not found", http.StatusNotFound)
+	}
+}
+
+// MustNewRR is a shortcut to dns.NewRR that panics on error.
+func MustNewRR(s string) dns.RR {
+	r, err := dns.NewRR(s)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+func dnsServe() {
+	z := "dns." + *flagHost
+	rrx := MustNewRR("$ORIGIN " + z + ".\n" + SOA)
 	dns.HandleFunc(z, func(w dns.ResponseWriter, r *dns.Msg) {
+		subnet := ""
+		for _, ext := range r.Extra {
+			opt, ok := ext.(*dns.OPT)
+			if !ok {
+				continue
+			}
+			for _, s := range opt.Option {
+				switch e := s.(type) {
+				case *dns.EDNS0_SUBNET:
+					subnet = e.String()
+				}
+			}
+		}
+
 		m := new(dns.Msg)
 		m.SetReply(r)
 		m.Authoritative = true
-		m.Ns = []dns.RR{rrx}
+
+		if strings.ToLower(r.Question[0].Name) == z+"." {
+			m.Answer = []dns.RR{rrx}
+		} else {
+			name := strings.TrimSuffix(r.Question[0].Name, ".")
+			dnsMap[name] = DNSData{
+				IP:         w.RemoteAddr().String(),
+				EdnsSubnet: subnet,
+				Expire:     time.Now().Add(2 * time.Minute),
+			}
+			rr := MustNewRR(name + CNAME)
+			m.Answer = []dns.RR{rr}
+		}
+		//fmt.Println(m)
 		w.WriteMsg(m)
 	})
 
 	go func() {
-		srv := &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "udp"}
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
 		if err := srv.ListenAndServe(); err != nil {
 			log.Fatalf("Failed to set udp listener %s\n", err.Error())
 		}
 	}()
 
 	go func() {
-		srv := &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "tcp"}
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "tcp"}
 		if err := srv.ListenAndServe(); err != nil {
 			log.Fatalf("Failed to set tcp listener %s\n", err.Error())
 		}
