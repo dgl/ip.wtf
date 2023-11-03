c1bd52d1de45ecfe04eef371a53eea75b3bd87bd
David Leadbeater <dgl@dgl.cx>
2023-11-04 08:40:23 +1100

    Reduce pledged things, as files are embedded now

--- main_openbsd.go
+++ main_openbsd.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"os"
-
 	"golang.org/x/sys/unix"
 )
 
 func sandboxSelf() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	unix.Unveil(cwd, "r")
 	unix.UnveilBlock()
-	unix.Pledge("stdio inet rpath", "")
+	unix.Pledge("stdio inet", "")
 }
